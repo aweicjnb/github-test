5d959a5a155e56ee0181934f86ec246641c8f628
awnb <3224159533@qq.com>
2022-07-16 21:15:57 +0800

    GitHub-test third commit

--- src/main/java/com/awnb/test/MyTest.java
+++ src/main/java/com/awnb/test/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hanp憨批");
         System.out.println("hanp憨批");
+        System.out.println("hanp憨批");
     }
 }
